8b0fe3e7225dde2022cd9aeb65e8b491875303b1
Gosqu248 <grzegorzurban248@gmail.com>
2024-06-26 14:19:42 +0200

    Home Page done

---
+++ src/main/java/pl/urban/korkpys/KorkPysApplication.java
@@ -0,0 +1,13 @@
+package pl.urban.korkpys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorkPysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KorkPysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/urban/korkpys/KorkPysApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.urban.korkpys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorkPysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
