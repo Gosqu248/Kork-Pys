c934185b9de4720c043b337557c970f3954267fc
Gosqu248 <grzegorzurban248@gmail.com>
2024-07-18 17:34:58 +0200

    Added rotation key

--- src/main/java/pl/urban/korkpys/security/JwtUtil.java
+++ src/main/java/pl/urban/korkpys/security/JwtUtil.java
@@ -4,11 +4,11 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import jakarta.annotation.PostConstruct;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
@@ -18,18 +18,14 @@ import java.util.function.Function;
 @Component
 public class JwtUtil {
 
-    @Value("${jwt.secret}")
-    private String secret;
-
     @Value("${jwt.expiration}")
     private Long expiration;
 
-    private Key signingKey;
+    private Key secretKey;
 
     @PostConstruct
     public void init() {
-        byte[] keyBytes = java.util.Base64.getDecoder().decode(secret);
-        this.signingKey = Keys.hmacShaKeyFor(keyBytes);
+        rotateKey(); // Generujemy pierwszy klucz
     }
 
     public String extractUsername(String token) {
@@ -46,7 +42,7 @@ public class JwtUtil {
     }
 
     private Claims extractAllClaims(String token) {
-        return Jwts.parserBuilder().setSigningKey(signingKey).build().parseClaimsJws(token).getBody();
+        return Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody();
     }
 
     private Boolean isTokenExpired(String token) {
@@ -59,17 +55,18 @@ public class JwtUtil {
     }
 
     private String createToken(Map<String, Object> claims, String subject) {
-        return Jwts.builder()
-                .setClaims(claims)
-                .setSubject(subject)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000))
-                .signWith(signingKey, SignatureAlgorithm.HS256)
-                .compact();
+                .signWith(secretKey, SignatureAlgorithm.HS256).compact();
     }
 
     public Boolean validateToken(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    // Metoda do rotacji kluczy
+    public void rotateKey() {
+        secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     }
 }
